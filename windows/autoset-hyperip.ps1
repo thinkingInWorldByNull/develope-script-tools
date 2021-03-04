2d6e628697df767d814cb970443fe32fcead5e14
EasonWang <18380802880@163.com>
2021-03-04 18:15:20 +0800

    add script

---
+++ windows/autoset-hyperip.ps1
@@ -0,0 +1,31 @@
+﻿<#The script will auto-set ip in host for Hyver-V#>
+
+$tempvar='D:\Helper\Hyper-V\ToolKit\temp.txt';
+get-vm -Name ubuntu-server-20 | Select -ExpandProperty Networkadapters | Select -Property  IPAddresses|Out-File -FilePath $tempvar;
+
+$inet4=(Get-Content -Path $tempvar| Select-String -Pattern '{' ).ToString();
+<#the host name in Hyper-V#>
+$newhostname='ubuntu';
+$newvmhost=$inet4.Substring(1,$inet4. IndexOf(',') - 1) + ' '+ $newhostname;
+
+if($newvmhost.Length -lt 12) {
+    return;
+}
+
+$hostpath='C:\Windows\System32\drivers\etc\hosts';
+<# consider backup it in other place#>
+$hostbackpath='C:\Windows\System32\drivers\etc\hosts.bak';
+
+
+$oldhosts=(Get-Content -Path $hostpath);
+$oldhosts|Out-File -FilePath $hostbackpath;
+
+$rewritehosts='';
+foreach($oldhost in $oldhosts) {
+    if(!$oldhost.Contains($newhostname)) {
+        $rewritehosts += $oldhost + "`r`n";
+    }
+}
+<#rewrite to hosts#>
+$rewritehosts + $newvmhost | Out-File -FilePath $hostpath;
+
---
+++ windows/autoset-ubuntu-id-local.ps1
@@ -0,0 +1,37 @@
+﻿<#This script will auto set host for remote computer, forexample if the remote hostname is easonwang-ubuntu#>
+
+<#this is very important , the name must be correct#>
+$local_ubuntu_ip_info=(nslookup easonwang-ubuntu)
+
+$local_ubuntu_ip_local;
+
+foreach($local_ubuntu_ip in $local_ubuntu_ip_info) {
+    if(!$local_ubuntu_ip.Contains('.')) {
+        continue;
+    }
+    $local_ubuntu_ip_local=$local_ubuntu_ip.Substring(10);
+}
+
+if($local_ubuntu_ip_local.Length -lt 10) {
+    return;
+}
+
+$hostpath='C:\Windows\System32\drivers\etc\hosts';
+<# consider backup it in other place#>
+$hostbackpath='C:\Windows\System32\drivers\etc\hosts.bak';
+
+
+$oldhosts=(Get-Content -Path $hostpath);
+$oldhosts|Out-File -FilePath $hostbackpath;
+
+<#The result is  ubunt 192.XXX, You can rename it as you like #>
+$newhostname='ubuntu';
+$rewritehosts='';
+
+foreach($oldhost in $oldhosts) {
+    if(!$oldhost.Contains($newhostname)) {
+        $rewritehosts += $oldhost + "`r`n";
+    }
+}
+$rewritehosts + $local_ubuntu_ip_local + ' ' + $newhostname| Out-File -FilePath $hostpath;
+
